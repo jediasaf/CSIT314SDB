287fc95676ce566b9b30fa92afa0859c66c6379d
krithikamayuri <mayurikrithika@gmail.com>
2023-05-04 20:12:31 +0800

    reviews
    edited 2 files
    entityClass.php
    doReview.php
    
    added the codes for the backend

--- doReview.php
+++ doReview.php
@@ -6,6 +6,9 @@ $movieTitle = $_POST['movietitle'];
 $star = $_POST['star'];
 $text = $_POST['text'];
 
-
+include "dbFunctions.php";
+$controller = new controller();
+$run = $controller -> run();
+$result = $run->addReview($text, $star);
 
 ?>
