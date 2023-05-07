c344c281004372d30d7edf197b0b5e167944cff9
Swetha0818 <coswetha24@gmail.com>
2023-05-07 22:03:25 +0800

    Update EditProfile.php
    Changes to EditProfile.php

--- EditProfile.php
+++ EditProfile.php
@@ -1,30 +1,67 @@
-<?php
-include ("../AdminNavBar.php");
-?>
-
-<!DOCTYPE html>
-<html lang="en">
+-<!doctype html>
+<html>
 <head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Edit User Profile</title>
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round|Open+Sans">
-<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<meta charset="UTF-8">
+<title>Edit Profile</title>
 <style type="text/css">
-@import url("CSS/AllUsers.css");
+@import url("CSS/EditProfile.css");
 </style>
 </head>
-
+<div class="hero">
+<?php include('navbar.php');?>
 <body>
+	<div class="box-form">
+	 <form class="edit-form">
+      <div class="container">
+       <img width="200px" src="Images/D.png">
+<h1>Edit Your Profile&nbsp;</h1>
+<h6>Kindly fill in this form to edit your profile.</h6>
+<label for="First Name"><b>First Name</b></label>
+        <input
+          type="text"
+          placeholder="Enter First Name"
+          name="first name"
+          id="firstname"
+          required
+        />
+		  
+		  <label for="Last Name"><b>Last Name</b></label>
+        <input
+          type="text"
+          placeholder="Enter Last Name"
+          name="last name"
+          id="last name"
+          required
+        />
+		  <label for="username"><b>Username</b></label>
+        <input
+          type="text"
+          placeholder="Enter username"
+          name="username"
+          id="username"
+          required
+        />
+		 
+<?php
 
+        <label for="email"><b>Email</b></label>
+        <input
+          type="text"
+          placeholder="Enter Email"
+          name="email"
+          id="email"
+          required
+        />
 # this page is meant for testing purposes only
 
+        <label for="pwd"><b>Password</b></label>
+        <input
+          type="password"
+          placeholder="Enter Password"
+          name="pwd"
+          id="pwd"
+          required
+        />
 class EditProfile{
   function display(){
     echo'
@@ -56,6 +93,61 @@ class EditProfile{
               required
             />
 
+        <label for="pwd-repeat"><b>Repeat Password</b></label>
+        <input
+          type="password"
+          placeholder="Repeat Password"
+          name="pwd-repeat"
+          id="pwd-repeat"
+          required
+        />
+		  <label for="Age"><b>Age</b></label>
+        <input
+          type="number"
+          placeholder="Enter Your Age"
+          name="age"
+          id="age"
+          required
+        />
+		 <label for="Preferences"><b>Preferences</b></label> <br>
+		  <select name="role" id="role">
+			<option value="No Preferences">Choose your preference</option>
+			<option value="Thriller">Thriller</option>
+			<option value="Adventure">Adventure</option>
+			<option value="Horror">Horror</option>
+			<option value="Fantasy">Fantasy</option>
+			<option value="Fantasy">Fantasy</option>
+			  <option value="Drama">Drama</option>
+			  <option value="Animation">Animation</option>
+			  <option value="Comedy">Comedy</option>
+			  <option value="Crime">Crime</option>
+			  <option value="Mystery">Mystery</option>
+			  <option value="Romance">Romance</option>
+			  <option value="Action">Action</option>
+			  <option value="Sci-Fi">Sci-Fi</option>
+			  <option value="Musical">Musical</option>
+			</select>
+		  <br>
+        <!-- submit button -->
+		  <a href="LoginPage.php">
+        <button type="submit">Register</button>
+		  </a>
+      </div>
+	</form>
+			</div>
+</body>
+<footer> 
+			<h3>Software Development Board 2023</h3><br>
+			<p>Pop-Up Cinema in Town</p>
+</footer>
+</html>
+	<script>
+		let subMenu = document.getElementById("subMenu");
+		
+		function toggleMenu(){
+			subMenu.classList.toggle("open-menu");
+		}
+	</script>
           <label for="Age"><b>Age</b></label>
             <input
               type="number"
@@ -110,4 +202,3 @@ class EditProfile{
 $display = new EditProfile();
 $display->display();
 ?>
-
