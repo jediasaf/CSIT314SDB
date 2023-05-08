28c240a4bf4de2129a2fb758e73f2017c918661c
jediasaf <85566380+jediasaf@users.noreply.github.com>
2023-05-08 16:09:30 +0800

    Update ManageBookings.php

--- ManageBookings.php
+++ ManageBookings.php
@@ -93,6 +93,7 @@ class displayManageBookings{
 <html lang="en">
 <head>
 
+
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -106,6 +107,9 @@ class displayManageBookings{
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <style type="text/css">
 @import url("CSS/AllUsers.css");
+center{
+background:white;
+}
 
 </style>';
 
