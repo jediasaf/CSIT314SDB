22f074e02b9d8a91ae49fb6235cddc99733b5d12
Bryead <bryantan3011@live.com>
2023-05-08 16:07:01 +0800

    edited 5 files
    ManageMovies.php
    edited a line for testing
    
    ManageShows.php
    set to boundary
    
    EditShow.php
    in the midst of coding edit and delete
    
    dbFunctions.php entityClass.php
    added 2 new functions

--- EditShow.php
+++ EditShow.php
@@ -1,58 +1,147 @@
 <?php
-include ("ManagerNavbar.php");
+
+
+class EditShow{
+  function display(){
+    include ("ManagerNavbar.php");
+    include ("dbFunctions.php");
+    $controller = new controller();
+    $message = "";
+
+    if($_SERVER['REQUEST_METHOD'] === 'GET'){
+      if($_GET['action'] == "edit"){
+          $message = '<center>
+          
+
+            
+          <form action="?" method="post" class="contact-form">
+
+
+          <div class="col-sm-5">
+              <div class="input-block">
+                <label for="">Room ID: </label>
+                <input type="text" name="roomID" value="'.$_GET['roomID'].'" class="form-control" readonly>
+              </div>
+            </div>
+
+
+          <div class="col-sm-5">
+              <div class="input-block">
+                <label for="">Movie ID: </label>
+                <input type="text" name="movieID" class="form-control">
+              </div>
+            </div>
+
+            <div class="col-sm-5">
+              <div class="input-block">
+                <label for="">Rows: </label>
+                <select type="text" name="rows" class="form-control">
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10</option>
+                </select>
+              </div>
+            </div>
+
+
+            <div class="col-sm-5">
+              <div class="input-block">
+                <label for="">Columns: </label>
+                <select type="text" name="columns" class="form-control">
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10</option>
+                </select>
+              </div>
+            </div>
+
+            <div class="col-md-12">
+            <div class="form-group">
+            <br><br>
+            <input type="submit" value="edit" name="submit" class="btn btn-primary">
+            <div class="submitting"></div>
+            </div>
+            </div>
+          </div>
+          </form>';
+      }
+      else{
+        $row = $controller->run("getRoomPlanFromID",$_GET['roomID']);
+        
+        $message = $message.'<h2>Reset the following for Room ID: '.$row[0]['roomID'].' to empty values?</h2><br><br>';
+        $message = $message.'<form action="?" method="post">';
+        $message = $message.'<h4>Movie ID: '.$row[0]['movieID'].'</h4>';
+        $message = $message.'<h4>Rows: '.$row[0]['rows'].'</h4>';
+        $message = $message.'<h4>Columns: '.$row[0]['columns'].'</h4>';
+        $message = $message.'<h4>Capacity: '.$row[0]['capacity'].'</h4>';
+        $message = $message.'<input type="hidden" name="roomID" value="'.$row[0]['roomID'].'" ><br><br>';
+        $message = $message.'<input type="submit" name="submit" value="reset">';
+        $message = $message.'</form>';
+        
+      }
+
+
+      
+    }
+    else{
+      if($_POST['submit'] == "edit"){
+        $roomID = $_POST['roomID'];
+        $tempmovieID = $_POST['movieID'];
+        $rows = $_POST['rows'];
+        $columns = $_POST['columns'];
+        $capacity = $rows * $columns;
+        $movieID = $controller -> run("escapeString",$tempmovieID);
+      }
+      else{
+        $roomID = $_POST['roomID'];
+      }
+
+      
+    }
+    echo '
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Manager Edit Show</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round|Open+Sans">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style type="text/css">
+    @import url("CSS/AllUsers.css");
+    </style>
+    </head>
+    
+    <body><div id="content">
+    <h2>Edit Show</h2>
+    </div>'
+    .$message.
+    ' </body>
+    </html>';
+
+  }
+}
+
+$display = new EditShow();
+$display -> display();
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Manager Edit Show</title>
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round|Open+Sans">
-<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<style type="text/css">
-@import url("CSS/AllUsers.css");
-</style>
-</head>
-
-<body>
-
-		<center>
-        <div id="content">
-		<h2>Edit <span style="color:#F8F8F8;"> Show</span></h2>
-		</div>
- <div class="form">
-            <form action="">
-                <div class="row">
-                    <div class="col">
-  <form action="" class="contact-form">
-	 <div class="col-sm-5">
-      <div class="input-block">
-        <label for="">Movie ID: </label>
-        <input type="text" class="form-control">
-      </div>
-    </div>
-	
-	<div class="col-sm-5">
-      <div class="input-block">
-        <label for="">Room ID: </label>
-        <input type="text" class="form-control">
-      </div>
-    </div>
-	
-    <div class="col-md-12">
-		<div class="form-group">
-		<input type="submit" value="Submit" class="btn btn-primary">
-		<div class="submitting"></div>
-		</div>
-		</div>
-  </div>
-</form>       
-</body>
-</html>
